9156fa175838657a21676f21fd6cf01162124bc5
Pledge Davis <pledgedavis14@gmail.com>
2020-01-29 01:03:49 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,20 +1,3 @@
-# class Person
-#   attr_accessor :partner, :name
-#
-#   def initialize(name)
-#     @name = name
-#   end
-#
-#   def get_married(person)
-#     self.partner = person
-#     person.partner = self
-#   end
-#
-# end
-#
-# beyonce = Person.new("Beyonce")
-# beyonce.get_married("Jay-Z")
-# puts beyonce.name
 
 
 class Person
@@ -46,4 +29,4 @@ end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
-puts beyonce.name 
+puts beyonce.name
